f72950292f7590dab5919d09bd9b9e1c9d5460bf
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2025-01-21 17:33:14 +0530

    Added APIG

---
+++ Cars365Apg/Cars365Apg/src/main/java/com/cars365/Cars365Apg/Cars365ApgApplication.java
@@ -0,0 +1,13 @@
+package com.cars365.Cars365Apg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cars365ApgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cars365ApgApplication.class, args);
+	}
+
+}
---
+++ Cars365Apg/Cars365Apg/src/test/java/com/cars365/Cars365Apg/Cars365ApgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars365.Cars365Apg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cars365ApgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
